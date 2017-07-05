1fba4e8ad70058aaa123d60f075806b84ef4d19c
谭宇 <tanyucn@outlook.com>
2017-07-05 21:35:12 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :groups do
+      member do
+        post :join
+        post :quit
+      end
+
+      resources :posts
+  end
+  root 'groups#index'
+
+
+  namespace :account do
+    resources :groups
+    resources :posts
+  end
 end
